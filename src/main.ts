9ca074bbf809245358d023e9a6f45479185058a9
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-03-15 18:28:09 +0700

    feat: login

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,32 @@ import { createGtm } from '@gtm-support/vue-gtm'
 import stores from './stores'
 import router from './router'
 import vuesticGlobalConfig from './services/vuestic-ui/global-config'
+import { initAuthStore } from './stores/auth.store'
+import { authMiddleware } from './router/router.middleware'
+import Notifications from '@kyvg/vue3-notification'
 import App from './App.vue'
 
-const app = createApp(App)
+const initApp = async () => {
+    authMiddleware()
+    await initAuthStore()
+    const app = createApp(App)
 
-app.use(stores)
-app.use(router)
-app.use(i18n)
-app.use(createVuestic({ config: vuesticGlobalConfig }))
+    app.use(stores)
+    app.use(router)
+    app.use(i18n)
+    app.use(createVuestic({ config: vuesticGlobalConfig }))
 
-if (import.meta.env.VITE_APP_GTM_ENABLED) {
-  app.use(
-    createGtm({
-      id: import.meta.env.VITE_APP_GTM_KEY,
-      debug: false,
-      vueRouter: router,
-    }),
-  )
+    if (import.meta.env.VITE_APP_GTM_ENABLED) {
+        app.use(
+            createGtm({
+                id: import.meta.env.VITE_APP_GTM_KEY,
+                debug: false,
+                vueRouter: router,
+            }),
+        )
+    }
+
+    app.use(Notifications)
+    app.mount('#app')
 }
-
-app.mount('#app')
+initApp()
