1fc4267bfed4e2ddf3c5d27d91bfc02b87cff64d
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2024-03-29 18:14:44 +0700

    chore: change indent from 2 to 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,13 +25,13 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'prettier/prettier': ['warn', {}, { usePrettierrc: true, indent: 2 }],
+        'prettier/prettier': ['warn', {}, { usePrettierrc: true, indent: 4 }],
         '@typescript-eslint/no-non-null-assertion': 0,
         '@typescript-eslint/no-explicit-any': 0, // allow explicit any's because of the legacy code and ts-less deps, but still prohibit IMplicit any's
         'vue/multi-word-component-names': 0,
         'vue/no-lone-template': 0,
         'vue/v-on-event-hyphenation': ['warn', 'never', { autofix: true }],
         'vue/component-name-in-template-casing': ['warn', 'PascalCase', { registeredComponentsOnly: false }],
-        'vue/script-indent': ['warn', 2], // , { baseIndent: 0 } - we should use that, but it didn't work for me for some reason.
+        'vue/script-indent': ['warn', 4], // , { baseIndent: 0 } - we should use that, but it didn't work for me for some reason.
     },
 }
